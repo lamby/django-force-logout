c0e5d6462e6a62b4ed75621064406401eedc188e
Chris Lamb <chris@chris-lamb.co.uk>
2016-03-21 01:03:30 +0800

    Use an int, not a datetime to avoid JSON serialisation issues (Closes: #1)

--- django_force_logout/middleware.py
+++ django_force_logout/middleware.py
@@ -1,3 +1,4 @@
+import time
 import datetime
 
 from django.contrib import auth
@@ -16,7 +17,7 @@ class ForceLogoutMiddleware(object):
 
         def callback(sender, user=None, request=None, **kwargs):
             if request:
-                request.session[self.SESSION_KEY] = datetime.datetime.utcnow()
+                request.session[self.SESSION_KEY] = int(time.time())
         auth.signals.user_logged_in.connect(callback, weak=False)
 
     def process_request(self, request):
@@ -29,7 +30,9 @@ class ForceLogoutMiddleware(object):
             return
 
         try:
-            timestamp = request.session[self.SESSION_KEY]
+            timestamp = datetime.datetime.utcfromtimestamp(
+                request.session[self.SESSION_KEY],
+            )
         except KeyError:
             # May not have logged in since we started populating this key.
             return
