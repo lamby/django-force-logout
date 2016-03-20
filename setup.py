3de1b0e08e17615c9a6b0b766db8829fec1ad3c9
Chris Lamb <chris@chris-lamb.co.uk>
2016-03-20 20:45:44 +0800

    Releasing version 2.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='django-force-logout',
 
     url="https://chris-lamb.co.uk/projects/django-force-logout",
-    version='2.0.0',
+    version='2.0.1',
     description="Framework to be able to forcibly log users out of Django projects",
 
     author="Chris Lamb",
